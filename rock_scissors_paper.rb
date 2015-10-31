cde8faac62064f0e00cd1495e512051b4fed95f3
kostik <kostik_g@ua.fm>
2015-10-31 18:31:38 +0200

    rock_scissors_paper was rubocoped, but still have 4 offencies

--- rock_scissors_paper.rb
+++ rock_scissors_paper.rb
@@ -3,51 +3,46 @@
 @computer_score = 0
 
 def player_choice
-	print "Enter your tern: "
-	@player = gets.strip.to_sym
+  print 'Enter your tern: '
+  @player = gets.strip.to_sym
 end
 
 def comp_choice
-	print "My tern is: "
-	@comp = @array[rand(3)]
-	puts "#{@comp}"
-end	
-
-def the_game 
-	if @player == @comp
-		puts "Draw"
-	elsif @player == :rock && @comp == :scissors
-		puts "Player wins"
-	elsif @player == :rock && @comp == :paper
-		puts "Computer wins"
-	elsif @player == :scissors && @comp == :rock
-		puts "Computer wins"
-	elsif @player == :scissors && @comp  == :paper
-		puts "Player wins"
-	elsif @player == :paper && @comp == :rock
-		puts "Player wins"
-	elsif @player == :paper && @comp == :scissors
-		puts "Computer wins"
-	end
+  print 'My tern is: '
+  @comp = @array[rand(3)]
+  puts "#{@comp}"
 end
 
 def the_game_case
-	case 
-  when @player == @comp then puts "Draw"
-  when @player == :rock && @comp == :scissors then puts "Player wins"; @player_score += 1
-  when @player == :rock && @comp == :paper then puts "Computer wins"; @computer_score += 1
-  when @player == :scissors && @comp == :rock then puts "Computer wins"; @computer_score += 1
-  when @player == :scissors && @comp  == :paper then puts "Player wins"; @player_score += 1
-  when @player == :paper && @comp == :rock then puts "Player wins"; @player_score += 1
-  when @player == :paper && @comp == :scissors then puts "Computer wins"; @computer_score += 1
-	end
+  case
+  when @player == @comp
+    puts 'Draw'
+  when @player == :rock && @comp == :scissors
+    puts 'Player wins'
+    @player_score += 1
+  when @player == :rock && @comp == :paper
+    puts 'Computer wins'
+    @computer_score += 1
+  when @player == :scissors && @comp == :rock
+    puts 'Computer wins'
+    @computer_score += 1
+  when @player == :scissors && @comp == :paper
+    puts 'Player wins'
+    @player_score += 1
+  when @player == :paper && @comp == :rock
+    puts 'Player wins'
+    @player_score += 1
+  when @player == :paper && @comp == :scissors
+    puts 'Computer wins'
+    @computer_score += 1
+  end
 end
 
 loop do
-  print "Press any key to continue plaing? (N - for exit) "
+  print 'Press any key to continue plaing? (N - for exit) '
 
-  if gets.strip.upcase == "N"
-    puts "Hope to see you again"
+  if gets.strip.upcase == 'N'
+    puts 'Hope to see you again'
     exit
   end
 
