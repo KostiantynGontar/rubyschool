5291cc44e6a36d42b41d105dd9f1f9cc307d472a
kostik <kostik_g@ua.fm>
2015-10-31 17:42:21 +0200

    simple calc

---
+++ calc.rb
@@ -0,0 +1,29 @@
+print "enter first number "
+a = gets.to_f
+
+print "choose you action (+-*/) "
+act = gets.strip
+
+print "enter last number "
+b = gets.to_f
+
+if act == "+"
+  result = a + b
+end
+
+if act == "-"
+  result = a - b
+end
+
+if act == "*"
+  result =  a * b
+end
+
+if act == "/" && b != 0
+  result = a / b
+else 
+  puts "You can't devide on 0"
+  exit
+end
+
+puts "result #{result}"
